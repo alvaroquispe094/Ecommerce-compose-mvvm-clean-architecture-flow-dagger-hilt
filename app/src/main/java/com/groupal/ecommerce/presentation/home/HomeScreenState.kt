784335ec8d8f2a146565655552529e8a4d7608ba
alvaroquispe094 <alvaroquispe094@gmail.com>
2022-05-19 17:01:18 -0300

    fix -> eliminando comentarios

--- app/src/main/java/com/groupal/ecommerce/presentation/home/HomeScreenState.kt
+++ app/src/main/java/com/groupal/ecommerce/presentation/home/HomeScreenState.kt
@@ -3,10 +3,9 @@ package com.groupal.ecommerce.presentation.home
 import com.groupal.ecommerce.domain.model.Category
 import com.groupal.ecommerce.domain.model.Product
 
-
 data class HomeScreenState(
     val isHomeOpen: Boolean = true,
-    val isLoading: Boolean = true,
+    val isLoading: Boolean = false,
     val products: List<Product> = emptyList(),
     val categories: List<Category> = emptyList(),
     val product: Product? = null,
