f1e5a65fd77b253ee68ac7749b71d97c69fe9b94
alvaroquispe094 <alvaroquispe094@gmail.com>
2023-01-25 12:58:05 -0300

    fix - missing files last push

---
+++ modules/configuration/configuration/src/main/java/com/groupal/configuration/ecommerce/domain/Token.kt
@@ -0,0 +1,5 @@
+package com.groupal.configuration.ecommerce.domain
+
+data class Token(
+    val accessToken: String
+)
---
+++ modules/user/user-data/src/main/java/com/groupal/user/ecommerce/data/utils/AuthAuthenticator.kt
@@ -0,0 +1,51 @@
+package com.groupal.user.ecommerce.data.utils
+
+import com.groupal.configuration.ecommerce.data.local.repository.TokenManager
+import com.groupal.user.ecommerce.data.network.AuthEndpoint
+import com.groupal.user.ecommerce.data.network.model.LoginResponseRest
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Inject
+
+class AuthAuthenticator @Inject constructor(
+    private val tokenManager: TokenManager,
+): Authenticator {
+
+    override fun authenticate(route: Route?, response: Response): Request? {
+        val token = runBlocking {
+            tokenManager.getToken().first()
+        }
+        return runBlocking {
+            val newToken = getNewToken(token)
+
+            if (!newToken.isSuccessful || newToken.body() == null) { //Couldn't refresh the token, so restart the login process
+                tokenManager.deleteToken()
+            }
+
+            newToken.body()?.let {
+                tokenManager.saveToken(it.accessToken)
+                response.request.newBuilder()
+                    .header("Authorization", "Bearer ${it.accessToken}")
+                    .build()
+            }
+        }
+    }
+
+    private suspend fun getNewToken(refreshToken: String?): retrofit2.Response<LoginResponseRest> {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        val okHttpClient = OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://crazy-gun-production.up.railway.app")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+        val service = retrofit.create(AuthEndpoint::class.java)
+        return service.refreshToken("Bearer $refreshToken")
+    }
+}
