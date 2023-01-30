7cd84d043bdbba3fc4af65e645f79ef8c2db9c99
alvaroquispe094 <alvaroquispe094@gmail.com>
2023-01-29 21:21:22 -0300

    feat - splash screen

--- app/src/main/java/com/groupal/ecommerce/presentation/MainActivity.kt
+++ app/src/main/java/com/groupal/ecommerce/presentation/MainActivity.kt
@@ -3,23 +3,34 @@ package com.groupal.ecommerce.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import com.groupal.ecommerce.di.ApplicationModule
 import com.groupal.ecommerce.presentation.navigation.AppNavigation
 import com.groupal.ecommerce.presentation.theme.EcommerceTheme
+import com.groupal.ecommerce.presentation.viewmodel.SplashScreenViewModel
 import com.groupal.shared.ecommerce.presentation.theme.DefaultThemeProvider
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private val viewModel: SplashScreenViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ApplicationModule.setApplicationScope(this.lifecycleScope)
 
+        // Splash screen
+        installSplashScreen().apply {
+            setKeepOnScreenCondition() {
+                viewModel.isLoading.value
+            }
+        }
         setContent {
             MyApp {
                 DefaultThemeProvider {
